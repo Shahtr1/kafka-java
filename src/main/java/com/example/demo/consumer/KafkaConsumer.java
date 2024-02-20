e649b4db82f59a43aa591c084e9ffd6e711c9dfb
Shahtr1 <tramboos123@gmail.com>
2024-02-20 18:26:05 +0530

    added json deserialization

--- src/main/java/com/example/demo/consumer/KafkaConsumer.java
+++ src/main/java/com/example/demo/consumer/KafkaConsumer.java
@@ -3,15 +3,23 @@ package com.example.demo.consumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.payload.Student;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "demo-topic", groupId = "myGroup")
+    // @KafkaListener(topics = "demo-topic", groupId = "myGroup")
     public void consumeMessage(String message) {
         log.info(String.format("Consuming the message from demo-topic:: %s", message));
+
+    }
+
+    @KafkaListener(topics = "demo-topic", groupId = "myGroup")
+    public void consumeJsonMessage(Student student) {
+        log.info(String.format("Consuming the student from demo-topic:: %s", student));
 
     }
 
--- src/main/java/com/example/demo/rest/MessageController.java
+++ src/main/java/com/example/demo/rest/MessageController.java
@@ -3,6 +3,8 @@ package com.example.demo.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.payload.Student;
+import com.example.demo.producer.KafkaJsonProducer;
 import com.example.demo.producer.KafkaProducer;
 
 import lombok.RequiredArgsConstructor;
@@ -15,12 +17,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/v1/messages")
 @RequiredArgsConstructor
 public class MessageController {
+
     private final KafkaProducer kafkaProducer;
+    private final KafkaJsonProducer kafkaJsonProducer;
 
     @PostMapping()
     public ResponseEntity<String> sendMessage(@RequestBody String message) {
         kafkaProducer.sendMessage(message);
-        return ResponseEntity.ok("Message queued");
+        return ResponseEntity.ok("Message queued successfully");
+    }
+
+    @PostMapping("/json")
+    public ResponseEntity<String> sendJsonMessage(@RequestBody Student message) {
+        kafkaJsonProducer.sendMessage(message);
+        return ResponseEntity.ok("Message queued successfully as JSON");
     }
 
 }
